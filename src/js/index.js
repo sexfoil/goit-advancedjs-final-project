9cfcdd30ea9ad1c8f328b2867da5e8c9b8537683
Slava Poliakov <78slavapolyakov@gmail.com>
2023-12-12 23:06:43 +0200

    Added urls property and api-call example

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,26 @@
-console.log("hello js");
+import axios from 'axios';
+import url from './property/url.js';
+
+/**
+ * import example
+ */
+const id = '64f389465ae26083f39b17a2';
+
+const testUrl = url.BASE_URL + url.EXERCISES + `/${id}`;
+
+async function getData() {
+    return axios.get(testUrl).then(result => result.data);
+}
+
+let data = await getData();
+
+console.log(data);
+document.querySelector('.test-api').innerHTML = getHtml(data);
+
+function getHtml(data) {
+    return `
+        <h3>Body part: ${data.bodyPart}</h3>
+        <h4>Description: ${data.description}</h4>
+        <h4>Raiting: ${data.rating}</h4>
+    `;
+}
---
+++ src/js/property/url.js
@@ -0,0 +1,10 @@
+const path = {
+    BASE_URL: 'https://your-energy.b.goit.study/api',
+    EXERCISES: '/exercises',
+    FILTERS: '/filters',
+    QUOTE: '/quote',
+    SUBSCRIPTION: '/subscription',
+    RATING: '/rating',
+}
+
+export default path;

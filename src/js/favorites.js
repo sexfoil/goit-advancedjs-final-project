08f8fd629c3db2afa27f72e6f1b528905c7b7490
Rostyslav Kovalchuk <rostykwave@gmail.com>
2023-12-15 17:03:58 +0200

    add pagination example

--- src/js/favorites.js
+++ src/js/favorites.js
@@ -1,20 +1,53 @@
 import { getExerciseCardHtml } from './utils/html-render';
 import { favoriteItem } from './localstorage';
+import { exercisesPagination } from './pagination/exercises-pagination';
 
-const favorites = document.querySelector('.exercises_content');
+///in APIs
+function getFavoriesExercises(page, limit) {
+  const arr = [
+    favoriteItem,
+    favoriteItem,
+    favoriteItem,
+    favoriteItem,
+    favoriteItem,
+    favoriteItem,
+    favoriteItem,
+    favoriteItem,
+    favoriteItem,
+  ];
 
-const arr = [
-  favoriteItem,
-  favoriteItem,
-  favoriteItem,
-  favoriteItem,
-  favoriteItem,
-  favoriteItem,
-  favoriteItem,
-  favoriteItem,
-  favoriteItem,
-];
-favorites.innerHTML = getExerciseCardHtml(arr);
-// const h = getExerciseCardHtml([]);
-// console.log(h);
-// favorites.innerHTML = h;
+  const totalItems = arr.lengh;
+  const startWith = (page - 1) * limit;
+  const endWith = page * limit;
+
+  const perPage = limit;
+  const totalPages = totalItems / limit;
+  const results = arr.slice[(startWith, endWith)];
+
+  return { page, perPage, totalPages, results };
+}
+
+function renderFavoriesExercises(page, limit) {
+  const favorites = document.querySelector('.exercises_content');
+
+  const data = getFavoriesExercises(page, limit);
+  favorites.innerHTML = getExerciseCardHtml(arr);
+
+  return data;
+
+  // const h = getExerciseCardHtml([]);
+  // console.log(h);
+  // favorites.innerHTML = h;
+}
+
+export { renderFavoriesExercises };
+
+///somewhere else in code
+//onClick in header
+const data = renderFavoriesExercises(1, 12);
+
+exercisesPagination.create({
+  totalPages: data.totalPages,
+  renderFunction: renderFavoriesExercises,
+  limit: 12,
+});

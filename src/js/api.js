61b51c6ba3f32402b748a8ccc8cd2c39f82d4965
Slava Poliakov <78slavapolyakov@gmail.com>
2023-12-15 18:21:52 +0200

    Doing css

--- src/js/api.js
+++ src/js/api.js
@@ -1,17 +1,77 @@
 import axios from 'axios';
 import url from './property/url.js';
 
-
-
-
-// const testUrl = url.BASE_URL + url.EXERCISES + `/${id}`;
-
-export default async function getExerciseById(id) {
-    return axios.get(getUrl(id))
-        .then(result => result.data)
-        .catch(err => err);
+export async function getExercisesByCategory(
+  page = 1,
+  limit = 12,
+  filter = 'Muscles'
+) {
+  const params = { filter, page, limit };
+  const fullUrl = url.BASE_URL + url.FILTERS + `?${getParameters(params)}`;
+  console.log(fullUrl);
+  return axios
+    .get(fullUrl)
+    .then(result => result.data)
+    .catch(err => err);
+}
+// https://your-energy.b.goit.study/api/filters?filter=Muscles&page=1&limit=12
+// https://your-energy.b.goit.study/api/filters?filter=Muscles&page=1&limit=12
+function getParameters(template) {
+  return new URLSearchParams(template);
 }
 
-function getUrl(id) {
-    return url.BASE_URL + url.EXERCISES + `/${id}`;
+// Перелік вправ з фільтрацією по категорії та ключовому слову з урахування кількості вправ у запиті та порядкового номеру сторінки
+export async function getExercisesName(
+  bodypart,
+  muscles,
+  equipment,
+  keyword,
+  page
+) {
+  return axios
+    .get(
+      url.BASE_URL +
+        url.EXERCISES +
+        `?bodypart=${bodypart}&muscles=${muscles}&equipment=${equipment}&keyword=${keyword}&page=${page}&limit=10`
+    )
+    .then(result => result.data)
+    .catch(err => err);
+}
+
+// запит за детальною інформаціею про вправу по id, буде приймати id обраної вправи
+export async function getExercisId(id) {
+  return axios
+    .get(url.BASE_URL + url.EXERCISES + `/${id}`)
+    .then(result => result.data)
+    .catch(err => err);
+}
+
+// запит за цитатою дня
+export async function getQuoteOfTheDay() {
+  return axios
+    .get(url.BASE_URL + url.QUOTE)
+    .then(result => result.data)
+    .catch(err => err);
+}
+
+// Додавання рейтингу окремій вправі
+export async function patchExercisReting(id, rate, email, review) {
+  return axios
+    .patch(url.BASE_URL + url.EXERCISES + `/${id}` + url.RATING, {
+      rate: rate,
+      email: email,
+      review: review,
+    })
+    .then(result => result.data)
+    .catch(Ererr => errror);
+}
+
+// Оформлення підписки на розсилку нових вправ
+export async function postSubscriptions(email) {
+  return axios
+    .post(url.BASE_URL + url.SUBSCRIPTION, {
+      email: email,
+    })
+    .then(result => result.data)
+    .catch(err => err);
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,6 @@
-import axios from 'axios';
-import url from './property/url.js';
-import getExerciseById from './api.js'
+import { getExerciseCardHtml } from './utils/html-render';
+import { getExercisesByCategory } from './api.js';
 
-
-/**
- * import example
- */
-// const id = '64f389465ae26083f39b17a2!!!';
-
-// const testUrl = url.BASE_URL + url.EXERCISES + `/${id}`;
-
-// async function getData() {
-//     return axios.get(testUrl)
-//         .then(result => result.data)
-//         .catch(err => err);
-// }
-
-// let data = await getData();
-
-// console.log(data);
-// document.querySelector('.test-api').innerHTML = getHtml(data);
-
-// function getHtml(data) {
-//     return `
-//         <h3>Body part: ${data.bodyPart}</h3>
-//         <h4>Description: ${data.description}</h4>
-//         <h4>Raiting: ${data.rating}</h4>
-//     `;
-// }
-
-const id = "asasas";
-const a = await getExerciseById('64f389465ae26083f39b17a2');
+const a = await getExercisesByCategory(1, 12);
 console.log(a);
+getExerciseCardHtml(a);
